c1ee14c1e3d8e125499f99bbb6da929f779c6b4c
ruimoliveira <ruimiguelalmeidaoliveira@gmail.com>
2019-12-29 16:28:02 +0000

    adds comments to reader class and starts execution

--- src/testPackage/MainClass.java
+++ src/testPackage/MainClass.java
@@ -3,6 +3,14 @@ package testPackage;
 public class MainClass {
 
 	public static void main(String[] args) {
+		int max_cycle_number = 10;
+		int tabu_list_size = 10;
+		int limit_divider = 2;
+		
+		run_experiment(max_cycle_number, tabu_list_size, limit_divider);
+	}
+
+	private static void run_experiment(int max_cycle_number, int tabu_list_size, int limit_divider) {
 		System.out.println("Hello world!");
 	}
 
--- src/utils/Reader.java
+++ src/utils/Reader.java
@@ -1,5 +1,4 @@
 package utils;
-
 import info.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -26,41 +25,56 @@ public class Reader {
 		try {
 			String line = br.readLine();
 			while (line != null) {
+				// aircraft_model
 				line = line.substring(line.indexOf('(')+1, line.indexOf(')'));
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// description
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// fleet
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// capacity
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// mtow
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// atc_avg_cost_nautical_mile
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// maintenance_avg_cost_minute
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// fuel_avg_cost_minute
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// airport_handling_cost
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// cpt
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// opt
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// scb
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// ccb
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// cab
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line);
+				
 				line = br.readLine();
 			}
 		} finally {
 			br.close();
 		}
 		while(!lines.isEmpty()) {
-			DATA.addAircraftModel(new AircraftModel(lines.get(0), lines.get(1), lines.get(2), Integer.valueOf(lines.get(3)), Integer.valueOf(lines.get(4)), Double.valueOf(lines.get(5)), Double.valueOf(lines.get(6)), Double.valueOf(lines.get(7)), Integer.valueOf(lines.get(8)), Integer.valueOf(lines.get(9)), Integer.valueOf(lines.get(10)), Integer.valueOf(lines.get(11)), Integer.valueOf(lines.get(12)), Integer.valueOf(lines.get(13))));
+			DATA.addAircraftModel(new AircraftModel(lines.get(0), lines.get(1), lines.get(2), Integer.valueOf(lines.get(3)), Integer.valueOf(lines.get(4)), Double.valueOf(lines.get(5)), Double.valueOf(lines.get(6)), Double.valueOf(lines.get(7)), Integer.valueOf(lines.get(8)), Integer.valueOf(lines.get(9)), Integer.valueOf(lines.get(10)),Integer.valueOf(lines.get(11)), Integer.valueOf(lines.get(12)), Integer.valueOf(lines.get(13))));
 			lines.remove(0);lines.remove(0);lines.remove(0);lines.remove(0);lines.remove(0);
 			lines.remove(0);lines.remove(0);lines.remove(0);lines.remove(0);lines.remove(0);
 			lines.remove(0);lines.remove(0);lines.remove(0);lines.remove(0);
@@ -73,12 +87,16 @@ public class Reader {
 		try {
 			String line = br.readLine();
 			while (line != null) {
+				// tail_number
 				line = line.substring(line.indexOf('(')+1, line.indexOf(')'));
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// aircraft_model_code
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// aircraft_model
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.length()-1));
+				
 				line = br.readLine();
 			}
 		} finally {
@@ -96,14 +114,19 @@ public class Reader {
 		try {
 			String line = br.readLine();
 			while (line != null) {
+				// iata_code
 				line = line.substring(line.indexOf('(')+1, line.indexOf(')'));
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// fleet
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// charge_type
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// charge
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(1, line.length()-1));
+				
 				line = br.readLine();
 			}
 		} finally {
@@ -121,29 +144,40 @@ public class Reader {
 		try {
 			String line = br.readLine();
 			while (line != null) {
+				// origin
 				if(line.indexOf("),") < line.indexOf(");"))
 					line = line.substring(line.indexOf('(')+1, line.indexOf(");"));
 				else
 					line = line.substring(line.indexOf('(')+1, line.indexOf("),"));
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// origin_description
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// origin_latitude
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// origin_longitude
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// destination
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// destination_description
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// destination_latitude
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// destination_longitude
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// distance_in_nautical_miles
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.indexOf(",")-1));
+				// distance_in_km
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(line.indexOf('\'')+1, line.length()-1));
+				
 				line = br.readLine();
 			}
 		} finally {
@@ -162,58 +196,85 @@ public class Reader {
 		try {
 			String line = br.readLine();
 			while (line != null) {
+				// event_id
 				line = line.substring(line.indexOf('(')+1, line.indexOf(')'));
 				lines.add(line.substring(0, line.indexOf(",")));
+				// event_time
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// event_type
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// event_cause
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// resource_affected
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// resource_type
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// resource_cap
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// crew_res_type
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// estimated_time_to_solve
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// flight_date
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// flight_number
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// scheduled_time_of_departure
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// scheduled_time_of_arrival
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// estimated_time_of_departure
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// estimated_time_of_arrival
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// origin
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// destination
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// departure_delay_in_minutes
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// bus_pax
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// econ_pax
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// total_pax
 				line = line.substring(line.indexOf(",")+1);
 				lines.add(line.substring(0, line.indexOf(",")));
+				// scheduled_trip_time
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// estimated_trip_time
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// scheduled_cost_aircraft
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// scheduled_cost_crew
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.indexOf(",")-1));
+				// scheduled_cost_passenger
 				line = line.substring(line.indexOf(",")+2);
 				lines.add(line.substring(0, line.length()-1));
+				
 				line = br.readLine();
 			}
 		} finally {
@@ -236,34 +297,49 @@ public class Reader {
 		try {
 			String line = br.readLine();
 			while (line != null) {
+				// flight_date
 				line = line.substring(line.indexOf('(')+2, line.indexOf(')'));
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// flight_number
 				line = line.substring(line.indexOf(",")+2, line.length());
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// origin
 				line = line.substring(line.indexOf(",")+2, line.length());
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// destination
 				line = line.substring(line.indexOf(",")+2, line.length());
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// flight_carrier
 				line = line.substring(line.indexOf(",")+2, line.length());
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// schedule_time_of_departure
 				line = line.substring(line.indexOf(",")+2, line.length());
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// schedule_time_of_arrival
 				line = line.substring(line.indexOf(",")+2, line.length());
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// bus_seats
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(0, line.indexOf(',')));
+				// econ_seats
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(0, line.indexOf(',')));
+				// total_seats
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(0, line.indexOf(',')));
+				// tail_number
 				line = line.substring(line.indexOf(",")+2, line.length());
 				lines.add(line.substring(0, line.indexOf('\'')));
+				// bus_seats_sold
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(0, line.indexOf(',')));
+				// econ_seats_sold
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line.substring(0, line.indexOf(',')));
+				// total_seats_sold
 				line = line.substring(line.indexOf(",")+1, line.length());
 				lines.add(line);
+				
 				line = br.readLine();
 			}
 		} finally {
